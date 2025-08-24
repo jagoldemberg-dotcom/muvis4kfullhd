97c5837c7494f7b89679f596f0439a128751b52b
jagoldemberg-dotcom <ja.goldemberg@duocuc.cl>
2025-08-24 15:13:43 -0400

    Feat: Se inicializa springboot app

---
+++ muvis4kfullhd/src/main/java/com/example/muvis4kfullhd/Muvis4kfullhdApplication.java
@@ -0,0 +1,13 @@
+package com.example.muvis4kfullhd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Muvis4kfullhdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Muvis4kfullhdApplication.class, args);
+	}
+
+}
---
+++ muvis4kfullhd/src/test/java/com/example/muvis4kfullhd/Muvis4kfullhdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.muvis4kfullhd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Muvis4kfullhdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
