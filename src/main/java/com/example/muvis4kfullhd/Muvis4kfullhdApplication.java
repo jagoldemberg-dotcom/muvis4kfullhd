54d007fc2403f042499b32c818e7e1ce86b9c654
jagoldemberg-dotcom <ja.goldemberg@duocuc.cl>
2025-08-24 22:32:42 -0400

    Update Muvis4kfullhdApplication.java

--- src/main/java/com/example/muvis4kfullhd/Muvis4kfullhdApplication.java
+++ src/main/java/com/example/muvis4kfullhd/Muvis4kfullhdApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Muvis4kfullhdApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Muvis4kfullhdApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Muvis4kfullhdApplication.class, args);
+    }
 
 }
